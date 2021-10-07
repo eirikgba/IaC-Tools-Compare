cd60b54db6b541bfbeb849dfbb19a5d1689c70c0
morterb <morterb@stud.ntnu.no>
2021-10-07 11:22:10 +0200

    Scripting

--- BashScipts/startScript.sh
+++ BashScipts/startScript.sh
@@ -8,7 +8,7 @@
 openstack server create \
 --flavor m1.small \
 --image 1676adb4-9657-42ed-b31f-b3907cbcd697 \
---key-name Manager \
+--key-name eirikgba-IaC \
 --security-group default \
 --user-data /home/ubuntu/iac/BashScipts/server-data.sh \
 webserver
