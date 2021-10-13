2e0947348fc2a5ba5f57e474b0fefea097dc05d0
morterb <morterb@stud.ntnu.no>
2021-10-13 12:00:43 +0200

    BashScripts server-data

--- BashScipts/startScript.sh
+++ BashScipts/startScript.sh
@@ -10,7 +10,7 @@ openstack server create \
 --image 1676adb4-9657-42ed-b31f-b3907cbcd697 \
 --key-name manager \
 --security-group default \
---network net1
+--network net1 \
 --user-data /home/ubuntu/iac/BashScipts/server-data.sh \
 webserver
 
