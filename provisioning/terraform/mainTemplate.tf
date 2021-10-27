f56ce33a1f76e5a693c9dd29f1df9dd1054926e5
morterb <morterb@stud.ntnu.no>
2021-10-27 10:25:51 +0200

    terraform

---
+++ provisioning/terraform/mainTemplate.tf
@@ -0,0 +1,22 @@
+# Define required providers
+terraform {
+required_version = ">= 0.14.0"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.35.0"
+    }
+  }
+}
+
+# Configure the OpenStack Provider
+provider "openstack" {
+  user_name   = "" #Service brukernavn
+  tenant_name = "" #prosjekt navn
+  password    = ""                            
+  auth_url    = "https://api.skyhigh.iik.ntnu.no:5000"
+  region      = "SkyHiGh"
+}
+
+
+
