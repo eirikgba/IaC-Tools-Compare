61c2db15741a2683acd479a87e43f0c1c4178e18
Eirik Granvin Bakke <eirikgba@stud.ntnu.no>
2021-11-07 11:48:28 +0000

    Update run.pp

--- configuration/docker/manifests/run.pp
+++ configuration/docker/manifests/run.pp
@@ -10,15 +10,32 @@ class docker::run {
                 ensure => file,
 		mode => '0664',
 		owner => 'ubuntu',
-                content => '<!DOCTYPE html>
-                        <html>
-                        <head>
-                            <title>IaC Project</title>
-                        </head>
-                        <body>
-                            <h1>Hello :D This is a website for IaC group 5</h1>
-                        </body>
-                        </html>',
+                content => '<!doctype html>
+						<html>
+						<head>
+						<title>Group 5 - IaC</title>
+						<meta charset="utf-8"/>
+						<meta name="robots" content="noindex"/>
+						<meta name="viewport" content="width=device-width, initial-scale=1.0">
+						<style>
+						body { text-align: center; padding: 150px; background: black;}
+						h1 { font-size: 50px; color:white; }
+						body { font: 20px Helvetica, sans-serif; color: white; }
+						article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+						a { color: #dc8100; text-decoration: none; }
+						a:hover { color: #333; text-decoration: none; }
+						</style>
+						</head>
+						<body>
+						<article>
+							<h1>Group 5 IaC project</h1>
+							<div>
+								<p>This web page is configured with puppet</p>
+							</div>
+						</article>
+						</body>
+						</html>
+						',
         }
 
 	exec { 'cp in right html file':
