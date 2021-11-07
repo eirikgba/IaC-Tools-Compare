8859283ed41b2d5d1a7dee98ed600dc907ec8ecf
Eirik Granvin Bakke <eirikgba@stud.ntnu.no>
2021-11-07 13:58:49 +0000

    Update install.pp

--- configuration/docker/manifests/install.pp
+++ configuration/docker/manifests/install.pp
@@ -36,22 +36,4 @@ class docker::install {
 	}
 
 
-#	exec { 'apt-get install docker-cd docker-ce-cli containerd.io':
-#		user => 'root',
-#		command => '/usr/bin/apt-get install docker-ce docker-ce-cli containerd.io',
-#		command => '/usr/bin/apt-get install ca-certificates curl gnupg lsb-release',
-#	}
-#
-#	exec { 'curl docker':
-#		user => 'root',	
-#                command => '/usr/bin/curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg',
-#	}
-#
-#
-
-#	exec { 'echo docker':
-#		user => 'root',
-#                command => '/usr/bin/echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable>',
-#	}
-
 }
